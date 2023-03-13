761774da6f793176d0ea640b1d41e677d320b6e9
WilliamMossD <Williammossd@gmail.com>
2023-03-13 17:24:31 +0000

    PHP Progress

--- assets/php/formHandler.php
+++ assets/php/formHandler.php
@@ -7,12 +7,18 @@
  * Handles form submissions
  */
 
+    // Validation Function
+    function validateInput($input, $expectedType, $expectedLength) {
+        return true;
+    }
+
     // Database Connection Info
     $HOST = '';
     $USER = 'mossfree_admin';
     $PASSWORD = 'Btf7@w&7Dhi1';
     $DATABASE = 'mossfree_';
 
+    // DEBUGGING ONLY
     echo(print_r($_POST));
 
     // Connect to database
@@ -23,6 +29,41 @@
         } 
     } catch (Exception $e) {
         echo "Unknown Error. Please try again";
+    }
+
+    try {
+        switch ($_POST["formID"]) {
+            // addUser Form - [firstNameInput, lastNameInput, emailInput] / [Text, Text, Email] / [50,50,100]
+            case "addUser":
+                break;
+            // editUser Form 
+            case "editUser":
+                break;
+            // deleteUser Form - [deleteUserSelect] / [Text] / [50]
+            case "deleteUser":
+                break;
+            // addModule Form - [moduleNameInput, ModuleConInput, ModuleDesInput, ModuleLinkInput] / [Text, Text, Text, URL] / [50, 50, 100, 2048]
+            case "addModule":
+                break;
+            // editModule Form
+            case "editModule":
+                break;
+            // deleteModule Form -  [deleteModuleSelect] / [Text] / [50]
+            case "deleteModule":
+                break;
+            // viewSession Form
+            case "viewSession":
+                break;
+            // addSession Form
+            case "addSession":
+                break;
+            // Unknown Form ID
+            default:
+                echo "Error: Unknown Form ID";
+                break;
+        }
+    } catch (Exception $e) {
+        echo "Unknown Error";
     }
 
 ?>
