aa25a6d209e08aa16ea7882b23f754f16eb620d0
WilliamMossD <Williammossd@gmail.com>
2023-04-07 22:24:51 +0100

    Logout Fix

--- public/logout.php
+++ public/logout.php
@@ -10,6 +10,6 @@
     // Start PHP Session 
     session_start();
     session_destroy();
-    header('Location: https://login.microsoftonline.com/common/oauth2/v2.0/logout');
+    header('Location: https://login.microsoftonline.com/common/oauth2/v2.0/logout?client_id=7b0feefa-d830-4aac-b302-ee754ac66acf&post_logout_redirect_uri=https%3A%2F%2Fwww.mossfreelancing.co.uk%2Findex.html');
     exit();
 ?>
